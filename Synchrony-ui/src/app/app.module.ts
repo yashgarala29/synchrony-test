32750e80ad9bb5486eee209225ef1877b4aeb041
Yash Garala <yashgarala29@gmail.com>
2025-02-05 00:13:53 +0530

    list added in UI

--- Synchrony-ui/src/app/app.module.ts
+++ Synchrony-ui/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './student-form/student-form.component';
 
+import {HttpClientModule } from '@angular/common/http';
+import { StudentListComponent } from './student-list/student-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
