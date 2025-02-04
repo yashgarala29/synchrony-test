fb420ab45cc3f6be34da94f098b6c96559a9aec9
Yash Garala <yashgarala29@gmail.com>
2025-02-04 22:15:07 +0530

    init

---
+++ Synchrony-service/src/test/java/com/test/Synchrony/SynchronyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.Synchrony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
